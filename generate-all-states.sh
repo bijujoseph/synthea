fa86b63accdbaa856b180c8e4cd9553f1ec6728f
Biju Joseph <biju.joseph@semanticbits.com>
2021-06-02 19:30:39 -0400

    tweak to generate script

--- generate-all-states.sh
+++ generate-all-states.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env sh
 ## Generates data for all states
-arrayx=('West Virginia' 'New Hampshire' 'New Jersey' 'District of Columbia'
-'New Mexico' 'New York' 'North Carolina' 'North Dakota'
-'Ohio' 'Oklahoma' 'Oregon' 'Pennsylvania' 'Rhode Island' 'South Carolina'
-'South Dakota' 'Alabama' 'Alaska' 'Arizona'
-'Arkansas' 'California' 'Colorado' 'Connecticut' 'Delaware'
- 'Florida' 'Georgia' 'Hawaii'
-'Idaho' 'Illinois' 'Indiana' 'Iowa' 'Kansas' 'Kentucky' 'Louisiana'
-'Maine' 'Maryland' 'Massachusetts' 'Michigan' 'Minnesota'
+array=('West Virginia' 'New Hampshire' 'New Jersey' 'District of Columbia' 'New Mexico' 'New York' 'North Carolina' 'North Dakota'
+'Ohio' 'Oklahoma' 'Oregon' 'Pennsylvania' 'Rhode Island' 'South Carolina' 'South Dakota' 'Alabama' 'Arizona'
+'Arkansas' 'California' 'Colorado' 'Connecticut' 'Delaware' 'Florida' 'Georgia' 'Hawaii'
+'Idaho' 'Illinois' 'Indiana' 'Iowa' 'Kansas' 'Kentucky' 'Louisiana' 'Maine' 'Maryland' 'Massachusetts' 'Michigan' 'Minnesota'
 'Mississippi' 'Missouri' 'Montana' 'Nebraska' 'Nevada' 'Tennessee' 'Texas' 'Utah' 'Vermont'
 'Virginia' 'Washington'  'Wisconsin' 'Wyoming')
 
-array=('Idaho')
+rm -rf $HOME/Downloads/synthea-output/
 
-for i in "${array[@]}"
+./run_synthea -p 400 -a 0-112 --exporter.baseDirectory $HOME/Downloads/synthea-output/ --exporter.csv.append_mode false "Alaska"
+
+for state in "${array[@]}"
 do
-  echo $i
- 	./run_synthea -p 1 -a 0-112 --exporter.baseDirectory /Users/biju.joseph/Downloads/synthea-output/ --exporter.csv.append_mode true "$i"
+  echo $state
+ 	./run_synthea -p 400 -a 0-112 --exporter.baseDirectory $HOME/Downloads/synthea-output/ "$state"
 done
